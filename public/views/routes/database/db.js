2c37d5c2aad24785c4120c3d15c57d41e438b287
dandandan1122 <dandiiorio12345@gmail.com>
2025-07-09 22:08:17 -0400

    Update db.js

--- public/views/routes/database/db.js
+++ public/views/routes/database/db.js
@@ -1,1 +1,8 @@
+// views/routes/database/db.js
+// Optional setup for future database logic
+function connectDatabase() {
+  console.log("🛠️ Placeholder: Database connected");
+}
+
+module.exports = { connectDatabase };
 
